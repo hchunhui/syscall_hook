bd41bfcf2dad5a6d41c300c5e788b1654850a9ea
Chunhui He <hchunhui@mail.ustc.edu.cn>
2021-05-30 08:10:44 +0000

    runtime: fix ld.so

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -20,6 +20,14 @@ callback(struct dl_phdr_info *info, size_t size, void *data)
 {
 	for (int j = 0; j < info->dlpi_phnum; j++) {
 		int p_type = info->dlpi_phdr[j].p_type;
+
+		/* XXX: handle ld.so */
+		unsigned long *signature0 = (void *) info->dlpi_phdr;
+		if (*signature0 == 0x4b4f4f48ff00aa55ul) {
+			*(signature0 + 1) = (unsigned long)(&wrapper);
+			continue;
+		}
+
 		unsigned long *signature = (void *) (info->dlpi_addr + info->dlpi_phdr[j].p_vaddr);
 		if (p_type == PT_LOAD && *signature == 0x4b4f4f48ff00aa55ul) {
 			*(signature + 1) = (unsigned long)(&wrapper);
