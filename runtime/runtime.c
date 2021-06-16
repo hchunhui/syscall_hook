e74c5b9afb651dc894219adae74b29824683e57a
Chunhui He <hchunhui@mail.ustc.edu.cn>
2021-06-16 14:15:20 +0000

    runtime: rewrite vfork/clone

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -2,6 +2,9 @@
 #include <link.h>
 #include <stddef.h>
 #include <sys/mman.h>
+#include <syscall.h>
+#include <sched.h>
+#include <errno.h>
 #include "syscall_hook.h"
 
 static int __thread reent = 0;
@@ -9,8 +12,29 @@ static int wrapper(struct frame *f)
 {
 	if (reent)
 		return 0;
+
 	reent++;
+
+	// for fork()/vfork()/clone():
+	// don't allow the child process to run in the same memory
+	// space, expect the TLS descriptor is properly set.
+	switch (f->nr_ret) {
+	case SYS_vfork:
+		f->nr_ret = SYS_fork;
+		break;
+	case SYS_clone:
+		if (f->args[0] & CLONE_VFORK) {
+			f->args[0] &= ~CLONE_VM;
+		} else if ((f->args[0] & CLONE_VM) &&
+			   !(f->args[0] & CLONE_SETTLS)) {
+			f->nr_ret = -EINVAL;
+			return 1;
+		}
+		break;
+	}
+
 	int ret = syscall_hook(f);
+
 	reent--;
 	return ret;
 }
